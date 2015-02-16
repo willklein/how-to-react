67aef66622e80075bbdb72093d540129a28ba223
Will Klein <will.klein@gmail.com>
2015-02-16 11:27:39 -0700

    Add tooling-demo w/ npm

---
+++ tooling-demo/data/events.js
@@ -0,0 +1,14 @@
+var eventsData = {
+  "events": [
+    {
+      "title": "Frontend Masters React Workshop Livestream",
+      "date": "2015-02-13",
+      "description": "Ryan Florence presents a workshop on React."
+    },
+    {
+      "title": "HTML5 Denver",
+      "date": "2015-02-16",
+      "description": "Learn about React and why tooling is awesome."
+    }
+  ]
+};
---
+++ tooling-demo/js/EventDetail.jsx
@@ -0,0 +1,22 @@
+var EventDetail = React.createClass({
+  render: function() {
+    console.log('EventDetail render');
+    var event = this.props.event;
+
+    return (
+      <div>
+        <h3>{ event.title }</h3>
+        <p><time datetime={ event.date }>{ event.date }</time></p>
+        <p>{ event.description }</p>
+      </div>
+    );
+  },
+
+  shouldComponentUpdate: function(nextProps, nextState) {
+    return nextProps.event !== this.props.event;
+  },
+
+  componentDidUpdate: function() {
+    console.log('EventDetail component did update!');
+  }
+});
